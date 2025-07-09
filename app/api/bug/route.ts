963efb2022e4dc8f857e358b4619f36a7c47043b
Fabrice7506 <sieguy7506@gmail.com>
2025-07-09 17:00:10 +0000

    MAJ

--- app/api/bug/route.ts
+++ app/api/bug/route.ts
@@ -1,49 +1,91 @@
-"use server"
-
-
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Authorization, Content-Type",
+    },
+  });
+}
 
+export async function POST(request: Request) {
+  try {
+    const apiKey = request.headers.get("Authorization")?.split(" ")[1];
 
-
-export async function POST(request : Request){
-
-    try{
-        const apiKey = request.headers.get("Authorization")?.split(" ")[1];
-
-        if(!apiKey){
-            return NextResponse.json({message:"Clé API requise."},{status:401})
+    if (!apiKey) {
+      return NextResponse.json(
+        { message: "Clé API requise." },
+        {
+          status: 401,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
         }
-
-        const {title,description,projectId} = await request.json();
-
-
-        if(!title||!description||!projectId){
-            return NextResponse.json({message:"titre , description et projectId sont requis"},{status:400});
-        }
-
-        const project = await prisma.project.findUnique({
-            where : {
-                id : projectId,
-                apiKey : apiKey
-            }
-        })
-
-        if(!project) return NextResponse.json({message:`le projet n'existe pas ${projectId}`},{status:404});
-
-        await prisma.bug.create({
-            data : {
-                title : title,
-                description : description,
-                projectId: projectId
-            }
-        })
-        return NextResponse.json({ message:"Bug enregistrée"},{status:201})
-
-    }catch(e){
-      
-        console.error(e);
-        return NextResponse.json({ message:"erreur interne du serveur"},{status:500})
+      );
     }
+
+    const { title, description, projectId } = await request.json();
+
+    if (!title || !description || !projectId) {
+      return NextResponse.json(
+        { message: "titre , description et projectId sont requis" },
+        {
+          status: 400,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+    }
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+        apiKey: apiKey,
+      },
+    });
+
+    if (!project)
+      return NextResponse.json(
+        { message: `le projet n'existe pas ${projectId}` },
+        {
+          status: 404,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+
+    await prisma.bug.create({
+      data: {
+        title: title,
+        description: description,
+        projectId: projectId,
+      },
+    });
+    return NextResponse.json(
+      { message: "Bug enregistrée" },
+      {
+        status: 201,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { message: "erreur interne du serveur" },
+      {
+        status: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+  }
 }
